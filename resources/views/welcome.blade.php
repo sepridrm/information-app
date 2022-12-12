5eb1615ea2857f9df36d3dcc26096cd507053113
Sepri Dermawan <sepridrm@MacBook-Air.local>
2022-12-12 16:51:18 +0700

    view percent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,63 +3,61 @@
 @section('title', 'BPKAD Muara Enim')
 
 @section('body')
-    <div class="wrapper bg-white d-flex align-items-center justify-content-center overflow-hidden">
+    <div class="wrapper bg-white d-flex align-items-center justify-content-center">
         <div class="row">
-            <div class="col-lg-8 col-8 pt-1">
-                <div class="row" style="height: 8.3vh">
-                    <div class="col-lg-1 col-1 text-center">
+            <div class="col-lg-8 col-8 d-flex flex-column">
+
+                <div class="row" style="height: 10%">
+                    <div class="col-lg-1 col-1 d-flex align-items-center justify-content-center">
                         <img class="img-fluid" src="vendor/adminlte/dist/img/merinem.png" style="width: 55px; height: 55px;">
                     </div>
 
-                    <div class="col-lg-11 col-11 d-flex align-items-center">
+                    <div class="col-lg-11 col-11 d-flex align-items-center justify-content-center">
                         <div class="row">
                             <div class="col-lg-3 col-3">
                                 <h4 class="mb-0">BPKAD Muara Enim</h4>
                             </div>
-                            <div class="col-lg-9 col-9 px-0">
-                                <marquee class="mb-0 h3">
-                                    Jadwal sholat hari ini {{ $schedule['tanggal'] }} untuk wilayah Kab. Muara Enim,
-                                    <strong>Subuh: {{ $schedule['subuh'] }} WIB</strong> | <strong>Dzuhur:
-                                        {{ $schedule['dzuhur'] }} WIB</strong> | <strong>Ashar: {{ $schedule['ashar'] }}
-                                        WIB</strong> | <strong>Magrib: {{ $schedule['maghrib'] }} WIB</strong> |
-                                    <strong>Isya: {{ $schedule['isya'] }} WIB</strong>
+                            <div class="col-lg-9 col-9">
+                                <marquee>
+                                    <h4 class="mb-0">
+                                        Jadwal sholat hari ini {{ $schedule['tanggal'] }} untuk wilayah Kab. Muara Enim, <strong>Subuh: {{ $schedule['subuh'] }} WIB</strong> | <strong>Dzuhur: {{ $schedule['dzuhur'] }} WIB</strong> | <strong>Ashar: {{ $schedule['ashar'] }} WIB</strong> | <strong>Magrib: {{ $schedule['maghrib'] }} WIB</strong> | <strong>Isya: {{ $schedule['isya'] }} WIB</strong>
+                                    </h4>
                                 </marquee>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="row" style="height: 11.5vh;">
-                    <marquee scrolldelay="100" class="d-flex bg-info px-2 py-5">
-                        <h2 class="mb-0">{{ $welcome->isi ?? 'Selamat Datang' }}</h2>
+                <div class="row d-flex align-items-center justify-content-center bg-info" style="height: 8%">
+                    <marquee scrolldelay="100">
+                        <h4 class="mb-0">{{ $welcome->isi ?? 'Selamat Datang' }}</h4>
                     </marquee>
                 </div>
 
-                <div class="row" style="height: 67vh;">
+                <div class="row" style="height: 80%">
                     @if (!$video->count() == 0)
-                        <video id="videoInformasi" style="width: 100%; margin: 0">
+                        <video id="videoInformasi" style="width: 100%; height: 100%; margin: 0">
                             {{-- <source src="{{ asset('storage') }}{{ substr($video[1]->path, 6) }}" type="video/mp4"> --}}
                         </video>
                     @endif
                 </div>
 
-                <div class="row" style="height: 11.8vh">
-                    <div
-                        class="col-lg-4 col-4 d-flex flex-column py-1 align-items-center justify-content-center bg-warning h3">
-                        {{ \Carbon\Carbon::parse(\Carbon\Carbon::now())->translatedFormat('l, d F Y') }}<span id="jam"
-                            style="font-size:24"></span>
+                <div class="row" style="height: 10%">
+                    <div class="col-lg-4 col-4 d-flex flex-column align-items-center justify-content-center bg-warning">
+                            <h4 class="mb-0">{{ \Carbon\Carbon::parse(\Carbon\Carbon::now())->translatedFormat('l, d F Y') }}</h4>
+                            <h4 class="mb-0" id="jam"></h4>
                     </div>
-                    <div class="col-lg-8 col-8 text-center bg-success py-1 px-0">
+                    <div class="col-lg-8 col-8 d-flex align-items-center justify-content-center bg-success">
                         <div class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner px-2 py-5">
+                            <div class="carousel-inner px-2">
                                 @foreach ($pengumuman as $item)
-                                    @if ($loop->index == 0)
+                                    @if($loop->index == 0)
                                         <div class="carousel-item active">
-                                            <h3 class="mb-0">{{ $item->isi }}</h3>
+                                            <h4 class="mb-0 text-center">{{ $item->isi }}</h4>
                                         </div>
                                     @else
                                         <div class="carousel-item">
-                                            <h3 class="mb-0">{{ $item->isi }}</h3>
+                                            <h4 class="mb-0 text-center">{{ $item->isi }}</h4>
                                         </div>
                                     @endif
                                 @endforeach
@@ -70,62 +68,62 @@
             </div>
 
             <div class="col-lg-4 col-4">
-                @if (Route::has('login'))
-                    <div class="hidden m-2 text-right" style="height: 7vh">
-                        <div class="py-2">
-                            <x-adminlte-button theme="dark" icon="fab fa-sm fa fa-arrows-alt"
-                                onclick="openFullscreen();" />
-                            <a href="{{ url('/home') }}">
-                                <x-adminlte-button theme="dark" icon="fab fa-sm fa fa-user" />
-                            </a>
+                <div class="row">
+                    <div class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner" style="height: 50vh">
+                            <div class="carousel-item active h-100">
+                                <img class="img-fluid h-100 w-100" src="img/slide1.jpeg" alt="First slide">
+                            </div>
+                            <div class="carousel-item h-100">
+                                <img class="img-fluid h-100 w-100" src="img/slide1.jpeg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item h-100">
+                                <img class="img-fluid h-100 w-100" src="img/slide1.jpeg" alt="Third slide">
+                            </div>
                         </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner" style="height: 50vh">
+                            @foreach ($image as $item)
+                                @if($loop->index == 0)
+                                    <div class="carousel-item active h-100">
+                                        <img class="img-fluid w-100 h-100" src="{{ asset('storage') }}{{ substr($item->path, 6) }}">
+                                    </div>
+                                @else
+                                    <div class="carousel-item h-100">
+                                        <img class="img-fluid w-100 h-100" src="{{ asset('storage') }}{{ substr($item->path, 6) }}">
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+
+                @if (Route::has('login'))
+                    <div class="hidden" style="position: absolute; top: 7px; right: 15px;">
+                        <x-adminlte-button theme="dark" icon="fab fa-sm fa fa-arrows-alt" onclick="openFullscreen();" />
+                        <a href="{{ url('/home') }}">
+                            <x-adminlte-button theme="dark" icon="fab fa-sm fa fa-user" />
+                        </a>
                     </div>
                 @endif
-
-                <div class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner" style="height: 44.7vh">
-                        <div class="carousel-item active">
-                            <img class="img-fluid w-100" src="img/slide1.jpeg" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="img-fluid w-100" src="img/slide1.jpeg" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="img-fluid w-100" src="img/slide1.jpeg" alt="Third slide">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="carousel slide mt-2" data-ride="carousel">
-                    <div class="carousel-inner" style="height: 45vh">
-                        @foreach ($image as $item)
-                            @if ($loop->index == 0)
-                                <div class="carousel-item active">
-                                    <img class="img-fluid w-100" src="{{ asset('storage') }}{{ substr($item->path, 6) }}">
-                                </div>
-                            @else
-                                <div class="carousel-item">
-                                    <img class="img-fluid w-100" src="{{ asset('storage') }}{{ substr($item->path, 6) }}">
-                                </div>
-                            @endif
-                        @endforeach
-                    </div>
-                </div>
             </div>
         </div>
     </div>
 
-    <div class="modal fade" id="notifModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="notifModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <h5 class="modal-title text-center mb-2" id="notifTitle"></h5>
-                    <video id="notifVideo" style="width: 100%; margin: 0">
-                        <source src="{{ asset('video') }}/adzan.mov" type="video/mp4">
-                    </video>
-                </div>
+          <div class="modal-content">
+            <div class="modal-body">
+                <h5 class="modal-title text-center mb-2" id="notifTitle"></h5>
+                <video id="notifVideo" style="width: 100%; margin: 0">
+                    <source src="{{ asset('video') }}/adzan.mov" type="video/mp4">
+                </video>
             </div>
+          </div>
         </div>
     </div>
 
@@ -156,7 +154,7 @@
         window.onload = function() {
             setJam();
             onHideNotif();
-            playVideo(videoUrl + videos[videoActive].path.substring(6));
+            playVideo(videoUrl+videos[videoActive].path.substring(6));
         }
 
         function setJam() {
@@ -167,15 +165,13 @@
             m = set(d.getMinutes());
             s = set(d.getSeconds());
 
-            e.innerHTML = h + ':' + m + ':' + s + ' WIB';
+            e.innerHTML =  h + ':' + m + ':' + s + ' WIB';
 
             jam = h + ':' + m + ':' + s
 
-            var adzan = arrSchedule.find((el) => {
-                return el.jam === jam;
-            });
-            if (adzan)
-                showNotif("Saatnya adzan " + adzan.nama);
+            var adzan = arrSchedule.find((el) => { return el.jam === jam; });
+            if(adzan)
+                showNotif("Saatnya adzan "+ adzan.nama);
 
             setTimeout('setJam()', 1000);
         }
@@ -185,19 +181,19 @@
             return e;
         }
 
-        notifVideo.addEventListener('ended', function(e) {
+        notifVideo.addEventListener('ended',function(e){
             $('#notifModal').modal('hide');
         });
 
-        videoInformasi.addEventListener('ended', function(e) {
+        videoInformasi.addEventListener('ended',function(e){
             videoActive++;
-            if (videoActive > videos.length - 1)
-                videoActive = 0
-            playVideo(videoUrl + videos[videoActive].path.substring(6));
+            if(videoActive > videos.length-1)
+                videoActive=0
+            playVideo(videoUrl+videos[videoActive].path.substring(6));
         });
 
-        document.addEventListener('keydown', function(e) {
-            if (e.key === "Escape")
+        document.addEventListener('keydown', function(e){
+            if(e.key === "Escape")
                 $(".hidden").show()
         })
 
@@ -206,15 +202,15 @@
             videoInformasi.play();
         }
 
-        function showNotif(title = null) {
+        function showNotif(title = null){
             $("#notifModal").modal()
             $("#notifTitle").text(title)
             videoInformasi.pause()
             notifVideo.play()
         }
 
-        function onHideNotif() {
-            $('#notifModal').on('hidden.bs.modal', function() {
+        function onHideNotif(){
+            $('#notifModal').on('hidden.bs.modal', function () {
                 notifVideo.pause()
                 videoInformasi.play()
             });
@@ -225,11 +221,9 @@
             $(".hidden").hide()
             if (elem.requestFullscreen) {
                 elem.requestFullscreen();
-            } else if (elem.webkitRequestFullscreen) {
-                /* Safari */
+            } else if (elem.webkitRequestFullscreen) { /* Safari */
                 elem.webkitRequestFullscreen();
-            } else if (elem.msRequestFullscreen) {
-                /* IE11 */
+            } else if (elem.msRequestFullscreen) { /* IE11 */
                 elem.msRequestFullscreen();
             }
         }
